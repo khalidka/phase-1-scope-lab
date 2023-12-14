a8fff9b15b4087bf890105fd24872fec0fc04f2d
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-14 05:05:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "original least favorite customer";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new least favorite customer";
+}
